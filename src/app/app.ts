03616cade97c95f9d5821eac80a741ce96b81f19
Matt Ma <bigmabig@gmail.com>
2016-01-07 11:40:22 -0800

    remove unused comment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,3 @@
-/*
- * Angular 2 decorators and services
- */
 import {Component} from 'angular2/core';
 import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import {FORM_PROVIDERS} from 'angular2/common';
@@ -48,12 +45,3 @@ export class App {
 
   }
 }
-
-/*
- * Please review the https://github.com/AngularClass/angular2-examples/ repo for
- * more angular app examples that you may copy/paste
- * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
- * For help or questions please contact us at @AngularClass on twitter
- * or our chat on Slack at https://AngularClass.com/slack-join
- * or via chat on Gitter at https://gitter.im/AngularClass/angular2-webpack-starter
- */
