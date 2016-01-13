57783577a52a7893013bb1a09b1db6e4cca45676
Matt Ma <bigmabig@gmail.com>
2016-01-12 22:17:48 -0800

    try with redux flow

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,12 +2,17 @@ import {provide} from 'angular2/core';
 import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {createStore} from 'redux';
+import {rootReducer} from './reducers/root.reducer';
+import {TodoActions} from './actions/todo.actions';
 
 /*
  * App Component
  * our top level component that holds all of our components
  */
-import {App} from './components/app/app';
+import {App} from './components/todoApp/app';
+
+const appStore = createStore(rootReducer);
 
 /*
  * Bootstrap our Angular app with a top level component `App` and inject
@@ -15,10 +20,12 @@ import {App} from './components/app/app';
  */
 document.addEventListener('DOMContentLoaded', function main() {
   bootstrap(App, [
-    ...('production' === process.env.ENV ? [] : ELEMENT_PROBE_PROVIDERS),
-    ...HTTP_PROVIDERS,
-    ...ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass: HashLocationStrategy})
+    // ...('production' === process.env.ENV ? [] : ELEMENT_PROBE_PROVIDERS),
+    // ...HTTP_PROVIDERS,
+    // ...ROUTER_PROVIDERS,
+    // provide(LocationStrategy, {useClass: HashLocationStrategy}),
+    provide('AppStore', { useValue: appStore }),
+    TodoActions
   ])
   .catch(err => console.error(err));
 });
