69dd634aba28959a105d4916310f0811bb948689
Matt Ma <bigmabig@gmail.com>
2016-01-17 19:48:38 -0800

    add redux dev tool for monitor the redux state

--- src/app/redux/stores/main-store.ts
+++ src/app/redux/stores/main-store.ts
@@ -1,11 +1,25 @@
 import {Injectable} from 'angular2/core';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import {BaseStore} from './base-store';
 import {TodoReducer} from '../reducers/todo';
 
+const isDevMode: boolean = (process.env.NODE_ENV === 'development');
 // Create ONLY one store per application
-let store = createStore(TodoReducer);
+let store;
+
+if (isDevMode) {
+  const createLogger = require('redux-logger');
+  // log only in dev mode
+  const logger = createLogger({
+    predicate: (getState, action) => isDevMode
+  });
+  const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+
+  store = createStoreWithMiddleware(TodoReducer);
+} else {
+  store = createStore(TodoReducer);
+}
 
 @Injectable()
 export class Store extends BaseStore {
