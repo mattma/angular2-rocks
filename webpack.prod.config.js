13a672f4eca0a162b9e8a3a08ecbeb911633acf7
Matt Ma <bigmabig@gmail.com>
2016-01-19 22:59:59 -0800

    add back the ts-loader ignoreDiagnostics config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -73,7 +73,13 @@ module.exports = {
           'compilerOptions': {
             'removeComments': true,
             'noEmitHelpers': true,
-          }
+          },
+          'ignoreDiagnostics': [
+            2403, // 2403 -> Subsequent variable declarations
+            2300, // 2300 -> Duplicate identifier
+            2374, // 2374 -> Duplicate number index signature
+            2375  // 2375 -> Duplicate string index signature
+          ]
         },
         exclude: [ /\.(spec|e2e)\.ts$/ ]
       },
