f69af9656ecf7ba2265800c3a2f7f5019ae6ed4c
Matt Ma <bigmabig@gmail.com>
2016-01-23 22:59:09 -0800

    add a redux localstorage with immutable data support

--- src/app/common/stores/main-store.ts
+++ src/app/common/stores/main-store.ts
@@ -1,5 +1,6 @@
 import {Injectable} from 'angular2/core';
-import {createStore, applyMiddleware, Store} from 'redux';
+import {compose, createStore, applyMiddleware, Store} from 'redux';
+import {List} from 'immutable';
 
 import {BaseStore} from './base-store';
 import Reducers from '../../components/todo/reducers';
@@ -10,13 +11,26 @@ let store: Store;
 
 if (isDevMode) {
   const createLogger = require('redux-logger');
+  const persistState = require('redux-localstorage');
+
   // log only in dev mode
   const logger = createLogger({
     predicate: (getState, action) => isDevMode
   });
-  const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+  const storage = persistState(null, {
+    key: 'rocks',
+    deserialize: (state) => {
+      if (state) {
+        const newState = JSON.parse(state);
+        return Object.assign({}, ...newState, {todos: List(newState.todos)});
+      }
+    }
+  });
 
-  store = createStoreWithMiddleware(Reducers);
+  const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+  const createPersistentStore = compose(storage)(createStoreWithMiddleware);
+
+  store = createPersistentStore(Reducers);
 } else {
   store = createStore(Reducers);
 }
