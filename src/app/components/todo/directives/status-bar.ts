29c83fb4d0acefa11c7c8ae0b059920a020a6eb2
Matt Ma <bigmabig@gmail.com>
2016-01-24 22:48:52 -0800

    correct the behavior of the status bar

--- src/app/components/todo/directives/status-bar.ts
+++ src/app/components/todo/directives/status-bar.ts
@@ -14,6 +14,6 @@ export class StatusBar {
 
   get remaining(): number {
     const state = this.store.getState();
-    return state.todos.filter(t => t.completed).toList().size;
+    return state.todos.filter(t => !t.completed).toList().size;
   }
 }
